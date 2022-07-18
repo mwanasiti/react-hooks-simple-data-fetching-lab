897881ae31a54610d41eca25d9b2ab1be378f231
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-07-18 09:47:47 +0300

    uvs

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogPhoto, setDogPhoto] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogPhoto(data.message);
+      });
+  }, []);
+
+  if (!dogPhoto) return <p>Loading...</p>;
+
+  return <img src={dogPhoto} alt="A Random Dog" />;
+}
+
+export default App;
